0a7496ec96d6fcfd74d436f001a41911086e706b
Ziad-d <ziadmo62@gmail.com>
2024-05-09 23:44:36 +0300

    CachedAttribute

---
+++ Stach.API/Helpers/CachedAttribute.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Stach.Domain.Services;
+using Stach.Service;
+using System.Text;
+
+namespace Stach.API.Helpers
+{
+    public class CachedAttribute : Attribute, IAsyncActionFilter
+    {
+        private readonly int _timeToLiveInSeconds;
+
+        public CachedAttribute(int timeToLiveInSeconds)
+        {
+            _timeToLiveInSeconds = timeToLiveInSeconds;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var responseCacheService = context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
+
+            var cacheKey = GenerateCacheKeyFromRequest(context.HttpContext.Request);
+
+            var response = await responseCacheService.GetCachedResponseAsync(cacheKey);
+
+            if(!string.IsNullOrEmpty(response))
+            {
+                var result = new ContentResult()
+                {
+                    Content = response,
+                    ContentType = "application/json",
+                    StatusCode = 200
+                };
+
+                context.Result = result;
+                return;
+            }
+
+            var executedActionContext = await next.Invoke();
+
+            if(executedActionContext.Result is OkObjectResult okObjectResult && okObjectResult.Value is not null)
+            {
+                await responseCacheService.CacheResponseAsync(cacheKey, okObjectResult.Value, TimeSpan.FromSeconds(_timeToLiveInSeconds));
+            }
+        }
+
+        private string GenerateCacheKeyFromRequest(HttpRequest request)
+        {
+            var keyBuilder = new StringBuilder();
+
+            keyBuilder.Append(request.Path);    // /api/products
+
+            foreach (var (key, value) in request.Query.OrderBy(x => x.Key)) 
+            {
+                keyBuilder.Append($"|{key}-{value}");
+            }
+
+            return keyBuilder.ToString();
+        }
+    }
+}
